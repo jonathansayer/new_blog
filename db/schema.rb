d402a851b42c3e67d03ef52449d15821efcf713e
jonathansayer <jon.sayer1@gmail.com>
2016-03-08 21:24:31 +0000

    removed author from articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306093622) do
+ActiveRecord::Schema.define(version: 20160308212230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20160306093622) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,9 +10,5 @@ RSpec.describe Article, :type => :model do
     it 'has a body' do
       expect(subject).to respond_to :body
     end
-
-    it 'has an author' do
-      expect(subject).to respond_to :author
-    end
   end
 end
