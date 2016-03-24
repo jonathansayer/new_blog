bc1cf4cbaa4bb95bb990767d52ea08d0d752c444
jonathansayer <jon.sayer1@gmail.com>
2016-03-24 20:43:31 +0000

    added admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+
+  namespace :admin do
+    get 'resources' => 'resources#index'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
